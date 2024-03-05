e8210f6bfa07001a04742a7dad038c5e17db9f7a
zakuro0w0 <tatatake0w0@gmail.com>
2024-03-05 14:47:01 +0900

    feat: fastapi | クエリパラメータ
    - pathパラメータでない引数はqueryパラメータになる
    - デフォルト値を与えたパラメータはOptionalになる
    - 以下の定義は同じ意味になる
      - param: int = None
      - param: Optional[int]
      - param: Union[int, None]
    - bool型のqueryパラメータの以下はTrueと扱われる
      - [1, "true", "True", "on", "yes"]

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from enum import Enum
+from typing import Optional
 
 app = FastAPI()
 
@@ -46,9 +47,26 @@ async def read_user(user_id: str):
     return {"user_id": user_id}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+def read_user_item(
+    user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 
 @app.put("/items/{item_id}")
