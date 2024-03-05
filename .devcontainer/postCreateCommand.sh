4dee271634e534ebf40cd9ff7826063d5bf867fb
zakuro0w0 <tatatake0w0@gmail.com>
2024-03-05 10:38:45 +0900

    build: fastapi, uvicornのインストール

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,3 +4,6 @@ cp .devcontainer/.bash_aliases ~/.bash_aliases
 ## ホスト側で使っていて、コンテナでも使いたいsshの設定をコピーする
 ## .devcontainer/.ssh/配下に予めconfigや秘密鍵を配置しておくこと
 cp .devcontainer/.ssh/* ~/.ssh/
+
+## python関連パッケージのインストール
+pip install -r requirements.txt
