46021752898102b5062945b73eef9b4e84ed4ec7
zakuro0w0 <tatatake0w0@gmail.com>
2024-03-26 17:59:49 +0900

    feat: タスクをpostした時、作成日時をfirestoreに登録する
    - pydanticのBaseModelを継承して作るリクエストデータの型について
      - リクエストモデルであるTaskInとレスポンスモデルであるTaskOutに分離
      - タスク生成日時はサーバ側で自動的に付与するのでリクエストbodyには不要
      - In/Outで分けるのはFastAPIの一般的な慣習らしいのでこれに倣う

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Union, Optional
+from pydantic import BaseModel, Field
+from typing import Optional
 import firebase_admin
 from firebase_admin import credentials, firestore
 import os
+from datetime import datetime
 
 app = FastAPI()
 
@@ -17,13 +18,42 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 
-class Task(BaseModel):
+class TaskIn(BaseModel):
+    """
+    タスクの入力モデルを表します。
+
+    属性:
+        title (str): タスクのタイトルです。
+        description (str): タスクの説明です。
+        completed (bool, optional): タスクが完了しているかどうかを示します。デフォルトはFalseです。
+    """
+
     title: str
     description: str
     completed: Optional[bool] = False
 
 
+class TaskOut(TaskIn):
+    """
+    タスクの出力モデルを表します。
+
+    TaskInクラスを継承し、createdAtフィールドを追加します。
+    """
+
+    createdAt: datetime = Field(default_factory=datetime.now)
+
+
 @app.post("/tasks/")
-async def create_task(task: Task):
-    doc_ref = db.collection("tasks").add(task.model_dump())
-    return {"id": doc_ref[1].id, "task": task}
+async def create_task(task: TaskIn):
+    """
+    データベースに新しいタスクを作成します。
+
+    Args:
+        task (TaskIn): 作成するタスク。
+
+    Returns:
+        dict: 作成されたタスクのIDとタスク自体を含む辞書。
+    """
+    task_out = TaskOut(**task.model_dump())
+    doc_ref = db.collection("tasks").add(task_out.model_dump())
+    return {"id": doc_ref[1].id, "task": task_out}
