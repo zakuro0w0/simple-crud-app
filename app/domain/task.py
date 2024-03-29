519567bd750ff6832468dc61318cf51ef5802b7c
zakuro0w0 <tatatake0w0@gmail.com>
2024-03-29 09:40:18 +0900

    fix: タスク生成日時と更新日時をJSTに合わせる

--- app/domain/task.py
+++ app/domain/task.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 
 class Task(BaseModel):
@@ -10,12 +11,16 @@ class Task(BaseModel):
     title: str - タスクのタイトル。
     description: str - タスクの説明。
     completed: bool - タスクの完了状態。デフォルトはFalse。
-    created_at: datetime - タスクの作成時刻。デフォルトは現在時刻。
-    updated_at: datetime - タスクの最終更新時刻。デフォルトは現在時刻。
+    created_at: datetime - タスクの作成時刻(JST)。デフォルトは現在時刻。
+    updated_at: datetime - タスクの最終更新時刻(JST)。デフォルトは現在時刻。
     """
 
     title: str
     description: str
     completed: bool = False
-    created_at: datetime = Field(default_factory=datetime.now)
-    updated_at: datetime = Field(default_factory=datetime.now)
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(ZoneInfo("Asia/Tokyo"))
+    )
+    updated_at: datetime = Field(
+        default_factory=lambda: datetime.now(ZoneInfo("Asia/Tokyo"))
+    )
